b25c4714a7a5728c354230c404c086ebfeb257c4
Lele Chen <lelechen@fb.com>
2021-04-19 14:06:21 -0400

    g

--- utils/utils.py
+++ utils/utils.py
@@ -53,7 +53,7 @@ def align_face(filepath, output_path, landmark_path = None ):
         lm = np.transpose(lm, (1, 0))
     lm = np.load(landmark_path)
     lm = np.transpose(lm, (1, 0))
-    lm = flip(lm, (0, 1))
+    lm = np.flip(lm, (0, 1))
     # print (lm,'+++')
     # lm = get_landmark(filepath)
     # print (lm,'===')
