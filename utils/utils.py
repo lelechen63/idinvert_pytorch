3cc7be4f2965af38ba5931c8731e5fe0d754f6b6
Lele Chen <lelechen@fb.com>
2021-04-19 23:29:45 -0400

    g

--- utils/utils.py
+++ utils/utils.py
@@ -118,17 +118,17 @@ def align_face(filepath, output_path, landmark_path = None ):
         mask = np.maximum(1.0 - np.minimum(np.float32(x) / pad[0], np.float32(w-1-x) / pad[2]), 1.0 - np.minimum(np.float32(y) / pad[1], np.float32(h-1-y) / pad[3]))
         blur = qsize * 0.02
         img = PIL.Image.fromarray(np.uint8(np.clip(np.rint(img), 0, 255)), 'RGB')
-        img.save('./gg.png'  )
-        img += (scipy.ndimage.gaussian_filter(img, [blur, blur, 0]) - img) * np.clip(mask * 3.0 + 1.0, 0.0, 1.0)
-        img = PIL.Image.fromarray(np.uint8(np.clip(np.rint(img), 0, 255)), 'RGB')
-        img.save('./gg2.png'  )
+        # img.save('./gg.png'  )
+        # img += (scipy.ndimage.gaussian_filter(img, [blur, blur, 0]) - img) * np.clip(mask * 3.0 + 1.0, 0.0, 1.0)
+        # img = PIL.Image.fromarray(np.uint8(np.clip(np.rint(img), 0, 255)), 'RGB')
+        # img.save('./gg2.png'  )
 
-        print (time.time() - d, '+++1')
+        # print (time.time() - d, '+++1')
         img += (np.median(img, axis=(0,1)) - img) * np.clip(mask, 0.0, 1.0)
-        print (time.time() - d, '++2z')
+        # print (time.time() - d, '++2z')
         img = PIL.Image.fromarray(np.uint8(np.clip(np.rint(img), 0, 255)), 'RGB')
         quad += pad[:2]
-        print (time.time() - d)
+        # print (time.time() - d)
 
 
     # Transform.
